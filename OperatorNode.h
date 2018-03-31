442e4e8de1bc1c5214957a0fb6b17d5f18de46aa
DeltaHarbinger <madmanpablo7722@gmail.com>
2018-03-31 09:06:08 -0500

    Added operator stack for conversion to postfix

---
+++ OperatorNode.h
@@ -0,0 +1,44 @@
+//
+// Created by Brandon Chung on 3/30/2018.
+//
+
+#ifndef DATASTRUCTURESPROJECTREDO_OPERATORNODE_H
+#define DATASTRUCTURESPROJECTREDO_OPERATORNODE_H
+
+
+class OperatorNode {
+
+private:
+	char data;
+	OperatorNode * nextNode;
+
+public:
+	OperatorNode(char data) : data(data) {
+		nextNode = nullptr;
+	}
+
+	OperatorNode() {
+		data = '';
+	}
+
+	char getData(){
+		return data;
+	}
+
+	void setData(char data){
+		this -> data = data;
+	}
+
+	OperatorNode * getNextNode(){
+		return nextNode;
+	}
+
+	void setNextNode(OperatorNode * nextNode){
+		this -> nextNode = nextNode;
+	}
+
+
+};
+
+
+#endif //DATASTRUCTURESPROJECTREDO_OPERATORNODE_H
---
+++ OperatorStack.h
@@ -0,0 +1,75 @@
+//
+// Created by Brandon Chung on 3/30/2018.
+//
+
+#ifndef DATASTRUCTURESPROJECTREDO_OPERATORSTACK_H
+#define DATASTRUCTURESPROJECTREDO_OPERATORSTACK_H
+
+#include "OperatorNode.h"
+
+class OperatorStack {
+private:
+	OperatorNode * top;
+
+public:
+
+	OperatorStack(){
+		top = nullptr;
+	}
+
+	char stackTop(){
+		if(top){
+			return top -> getData();
+		} else {
+			throw -1;
+		}
+	}
+
+	void pop(){
+		if(top){
+			OperatorNode * temp = top;
+			top = top -> getNextNode();
+			delete temp;
+		} else {
+			throw -2;
+		}
+	}
+
+	void push(char data){
+		if(OperatorNode * temp = new OperatorNode(data)){
+			temp -> setNextNode(top);
+			top = temp;
+		}
+	}
+
+	int count(){
+		if(!top){
+			return 0;
+		}
+		int count = 0;
+		OperatorStack * tempOperatorStack = new OperatorStack();
+		while(top){
+			count++;
+			tempOperatorStack -> push(this -> stackTop());
+			this -> pop();
+		}
+
+		for(int x = 0; x < count; x++){
+			this -> push(tempOperatorStack -> stackTop());
+			tempOperatorStack -> pop();
+		}
+
+		return count;
+	}
+
+	void empty(){
+		while(top){
+			OperatorNode * temp = top;
+			top = top -> getNextNode();
+			delete temp;
+		}
+	}
+};
+
+
+#endif //DATASTRUCTURESPROJECTREDO_OPERATORSTACK_H
